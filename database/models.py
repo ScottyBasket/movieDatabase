6768c8368174c0c668a0aafa4b6efec9849f7d57
ScottyBasket <eli.bassett@gmail.com>
2023-10-29 16:19:35 -0400

    trial 1, name only

--- database/models.py
+++ database/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import Q
+from django.utils.timezone import now
+from django.utils.translation import gettext_lazy as _
+from operator import itemgetter
+
+MAX_NAME_LEN = 64
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+
+    def __str__(self):
+        return self.name
