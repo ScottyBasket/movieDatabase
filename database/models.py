848f4bac5abbd2c8ea67c1f2eb39278a904a5631
ScottyBasket <eli.bassett@gmail.com>
2023-10-29 18:25:29 -0400

    backend set up attempt

--- database/models.py
+++ database/models.py
@@ -14,7 +14,15 @@ MAX_NAME_LEN = 64
 
 
 class Movie(models.Model):
-    name = models.CharField(max_length=255, blank=False)
+    name = models.CharField('Name', max_length=255, unique=False, blank=False)
+    barcode = models.IntegerField(
+        'Barcode', null=True)
+    year = models.DateField('Date', null=True)
+    genre = models.CharField('Genre', max_length=255, null=True)
+    rating = models.IntegerField('Rating', null=True)
+    runTime = models.IntegerField('Run Time', null=True)
+    director = models.CharField('Director', max_length=255, null=True)
+    description = models.CharField('Description', max_length=255, null=True)
 
     def __str__(self):
         return self.name
